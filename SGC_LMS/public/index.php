7b3b033666d73e5d28d793a5629453cfae79aa17
amichiamine <amichi.amine@gmail.com>
2025-08-19 14:19:47 +0100

    Add SGC_LMS/public/index.php 1755609589032

---
+++ SGC_LMS/public/index.php
@@ -0,0 +1,11 @@
+<?php
+// Point d'entrée principal de l'application SGC_LMS
+
+session_start();
+
+// Pour l'instant, on charge directement la page d'accueil
+// (auth.php sera ajouté en Phase 3)
+
+$content = file_get_contents('../templates/pages/home.php');
+include '../templates/layouts/main-updated.php';
+?>
